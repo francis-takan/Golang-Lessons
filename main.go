bd5c3bf26793cf252709a827b116eec6f617c186
opeCre8tivia <opegude.n3t@gmail.com>
2024-09-14 13:25:42 +0300

    chanded add function to return x and y

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	fmt.Println("Hello there...")
 
 	fmt.Println(add(2, 5))
 }
 
-func add(x int, y int)  int {
-	num1 := 1
-	num2 := 2
-	return num1 + num2
+func add(x int, y int) int {
+	return x + y
 }
